5204b5c81e987d0ccb3d917ea908c898d52bef48
achaval-tomas <tomiachaval2013@gmail.com>
2024-01-21 04:15:43 -0300

    Created Auth Service

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,16 @@
+// login exceptions
+class UserNotFoundAuthException implements Exception {}
+
+class WrongCredentialAuthException implements Exception {}
+
+// register exceptions
+class EmailAlreadyInUseAuthException implements Exception {}
+
+class WeakPasswordAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
+
+// generic exceptions
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
