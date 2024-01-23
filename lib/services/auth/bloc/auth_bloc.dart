f3d3d6240ce2792ab701e9af1c67baa9f9ac0d11
achaval-tomas <tomiachaval2013@gmail.com>
2024-01-23 00:15:18 -0300

    Handling auth exceptions in Login View

--- lib/services/auth/bloc/auth_bloc.dart
+++ lib/services/auth/bloc/auth_bloc.dart
@@ -11,7 +11,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState>{
         await provider.initialize();
         final user = provider.currentUser;
         if (user == null) {
-          emit(const AuthStateLoggedOut());
+          emit(const AuthStateLoggedOut(null));
         } else if (!user.isEmailVerified){
           emit(const AuthStateNeedsVerification());
         } else {
@@ -22,7 +22,6 @@ class AuthBloc extends Bloc<AuthEvent, AuthState>{
     // LOG IN
     on<AuthEventLogIn>(
       (event, emit) async {
-        emit(const AuthStateLoading());
         final email = event.email;
         final password = event.password;
         try {
@@ -32,7 +31,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState>{
           );
           emit(AuthStateLoggedIn(user));
         } on Exception catch (e) {
-          emit(AuthStateLoginFailure(e));
+          emit(AuthStateLoggedOut(e));
         }
       }
     );
@@ -42,7 +41,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState>{
         try {
           emit(const AuthStateLoading());
           await provider.logOut();
-          emit(const AuthStateLoggedOut());
+          emit(const AuthStateLoggedOut(null));
         } on Exception catch (e) {
           emit(AuthStateLogoutFailure(e));
         }
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -4,6 +4,7 @@ import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_exceptions.dart';
 import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
 import 'package:mynotes/services/auth/bloc/auth_event.dart';
+import 'package:mynotes/services/auth/bloc/auth_state.dart';
 import 'package:mynotes/utilities/dialogs/error_dialog.dart';
 
 class LoginView extends StatefulWidget {
@@ -14,7 +15,6 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
-
   late final TextEditingController _email;
   late final TextEditingController _password;
 
@@ -31,7 +31,7 @@ class _LoginViewState extends State<LoginView> {
     _password.dispose();
     super.dispose();
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,58 +40,51 @@ class _LoginViewState extends State<LoginView> {
       ),
       body: Column(
         children: [
-      
           TextField(
             controller: _email,
             enableSuggestions: false,
             autocorrect: false,
             keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-            hintText: 'myemail@example.com'
-            ),
+            decoration: const InputDecoration(hintText: 'myemail@example.com'),
           ),
-      
           TextField(
             controller: _password,
             obscureText: true,
             enableSuggestions: false,
             autocorrect: false,
-            decoration: const InputDecoration(
-            hintText: 'Enter your password'
-            ),
+            decoration: const InputDecoration(hintText: 'Enter your password'),
           ),
-      
-          TextButton(
-            onPressed:() async {
-      
-              final email = _email.text;
-              final password = _password.text;
-      
-              try {
-                context.read<AuthBloc>().add(
-                  AuthEventLogIn(email, password)
-                );
-              } on WrongCredentialAuthException { 
-                await showErrorDialog(
-                    context,
-                    'Incorrect email or password.'
-                  );
-              } on GenericAuthException {
+          BlocListener<AuthBloc, AuthState>(
+            listener: (context, state) async {
+              if (state is AuthStateLoggedOut) {
+                if (state.exception is WrongCredentialAuthException) {
                   await showErrorDialog(
                     context,
-                    'Authentication Error.',
+                    'Wrong email or password.'
                   );
+                } else if (state.exception is GenericAuthException) {
+                  await showErrorDialog(
+                    context,
+                    'Authentication Error.'
+                  );
+                }
               }
-            }, 
-            child: const Text('Log In')
+            },
+            child: TextButton(
+                onPressed: () async {
+                  final email = _email.text;
+                  final password = _password.text;
+                  context.read<AuthBloc>()
+                        .add(AuthEventLogIn(email, password));
+                },
+                child: const Text('Log In')
+            ),
           ),
           TextButton(
             onPressed: () {
-              Navigator.of(context).pushNamedAndRemoveUntil(
-                registerRoute,
-                (_) => false
-              );
-            }, 
+              Navigator.of(context)
+                  .pushNamedAndRemoveUntil(registerRoute, (route) => false);
+            },
             child: const Text("Don't have an account? Register here!"),
           )
         ],
